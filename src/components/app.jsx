a98bea23e1374d35937cff9f41448e149875b185
camille breillot <camille.breillot@gmail.com>
2022-01-19 18:57:19 +0100

    giflist component with array of props defined in app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,14 @@
 import React,  { Component } from 'react';
 import Search from './search_bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
   render () {
+    const gifs = [
+      { id: "aNJi783l8MnTtxIKWl" },
+      { id: "ANrbXQ5RtWYpjDhvfN" }
+    ];
     return (
       <div>
         <div className="left-scene">
@@ -13,6 +18,7 @@ class App extends Component {
         </div>
         </div>
         <div className="right-scene">
+          <GifList gifs={gifs} />
         </div>
       </div>
     );
