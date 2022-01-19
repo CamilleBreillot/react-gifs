2ad361557188e0fe677c06163d2aa3225e43109b
camille breillot <camille.breillot@gmail.com>
2022-01-19 18:20:22 +0100

    search bar component created and implemented with basic render

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,15 @@
 import React,  { Component } from 'react';
+import Search from './search_bar.jsx';
 
 class App extends Component {
   render () {
     return (
       <div>
-        <div className="left-scene" />
-        <div className="right-scene" />
+        <div className="left-scene">
+          <Search />
+        </div>
+        <div className="right-scene">
+        </div>
       </div>
     );
   }
---
+++ src/components/search_bar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <input className='form-search form-control' type='text'></input>
+    );
+  }
+}
+
+export default Search;
