3cae89813fd6ceed0ed407487a2bc9d93930943b
camille breillot <camille.breillot@gmail.com>
2022-01-20 10:49:56 +0100

    UI improved with box shadown on hover/focus input

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -7,7 +7,7 @@ class Search extends Component {
 
   render() {
     return (
-      <input className='form-search form-control' type='text'
+      <input className='form-search form-control' type='text' placeholder='What gifs are you looking for?'
       onChange={this.handleUpdate}></input>
     );
   }
